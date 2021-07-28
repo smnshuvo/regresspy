a4502273827ffa8967f93c6e7bee8effe5afcd07
mhsammak <musabbirhasansammak@outlook.com>
2021-07-28 13:05:10 +0600

    Completes template

--- test/test_loss.py
+++ test/test_loss.py
@@ -1,8 +1,13 @@
 import numpy as np
 
+from regresspy.loss import mae, sse, mse, rmse
+
 
 def test_mae():
-    pass
+    dummy_predictions = np.array([2.0, 4.0, 7.0, 8.0])
+    dummy_labels = np.array([1.0, 3.0, 6.0, 7.0])
+    dummy_mae = 1.0
+    assert mae(dummy_predictions, dummy_labels) == dummy_mae
 
 
 def test_sse():
