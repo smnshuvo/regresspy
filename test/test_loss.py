4c670474177e00db0702fbadbf0f1a2aff69c56d
Shuvo <smnshuvo@gmail.com>
2021-08-13 22:08:21 +0600

    wrote functions for test_loss.py

--- test/test_loss.py
+++ test/test_loss.py
@@ -11,12 +11,21 @@ def test_mae():
 
 
 def test_sse():
-    pass
+    dummy_predictions = np.array([2.0, 4.0, 7.0, 8.0])
+    dummy_labels = np.array([1.0, 3.0, 6.0, 7.0])
+    dummy_sse = 4.0
+    assert sse(dummy_predictions, dummy_labels) == dummy_sse
 
 
 def test_mse():
-    pass
+    dummy_predictions = np.array([2.0, 4.0, 7.0, 8.0])
+    dummy_labels = np.array([1.0, 3.0, 6.0, 7.0])
+    dummy_mse = 1.0
+    assert mse(dummy_predictions, dummy_labels) == dummy_mse
 
 
 def test_rmse():
-    pass
+    dummy_predictions = np.array([2.0, 4.0, 7.0, 8.0])
+    dummy_labels = np.array([1.0, 3.0, 6.0, 7.0])
+    dummy_rmse = 1.0
+    assert rmse(dummy_predictions, dummy_labels) == dummy_rmse
